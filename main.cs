3d79864d22cad48250478d6c9eb92c7e3e827c91
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-25 22:59:39 +0000

    edited code

--- main.cs
+++ main.cs
@@ -1,17 +1,28 @@
 // Created by: Zaida Hammel
 // Created on: April 2022
 //
-// This program accepts user inputusing System;
+// This program accepts user input
 
 using System;
 
-class RandomGenerator 
+class Program
 {
-    public static void Main (string[] args) 
+    public static void Main(string[] args)
     {
-        System.Random random = new System.Random();
-        // This function determines if your guess is correct.
-        System.Console.WriteLine(random.Next(6));
-
+        Random random = new Random();
+        int returnNumber = random.Next(1, 6);
+        int guess = 0;
+        Console.WriteLine("Pick a number from 1-6 and guess what the randomized number is.");
+        if (guess != returnNumber)
+        {
+            guess = Convert.ToInt32(Console.Read());
+            Console.WriteLine("That's too bad, you got it wrong! Try again. The answer was " + returnNumber + ".");
+            Console.ReadLine();
+        }
+        if (guess == returnNumber)
+        {
+            Console.WriteLine("You guessed it! It was " + returnNumber + ".");
+            Console.ReadLine();
+        }
     }
 }
