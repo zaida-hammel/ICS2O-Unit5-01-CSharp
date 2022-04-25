1aebf4205b69774d2edb857da1c8f05130428b4f
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-25 22:53:00 +0000

    added files

--- main.cs
+++ main.cs
@@ -1,7 +1,17 @@
+// Created by: Zaida Hammel
+// Created on: April 2022
+//
+// This program accepts user inputusing System;
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class RandomGenerator 
+{
+    public static void Main (string[] args) 
+    {
+        System.Random random = new System.Random();
+        // This function determines if your guess is correct.
+        System.Console.WriteLine(random.Next(6));
+
+    }
 }
